29a82788c0de86fdfb9c430404dc4b41a9d187d0
Marcus Westin <marcus@sekurbit.se>
2023-09-19 11:42:14 +0200

    altered implementation to suit us

--- src/main/java/org/thingsboard/mapper/controller/CustomOAuth2MapperController.java
+++ src/main/java/org/thingsboard/mapper/controller/CustomOAuth2MapperController.java
@@ -47,51 +47,26 @@ public class CustomOAuth2MapperController {
         }
         OAuth2User result = new OAuth2User();
 
-        // In case of PE please use PEOAuth2User
-        // PEOAuth2User result = new PEOAuth2User();
+        // Email addresses from Azure B2C are retrieved in an array, where we pick the
+        // first email address as the user login address.
+        JsonNode emailAddresses = externalUserInJson.get("emails");
+        if (emailAddresses.isArray()) {
+            Iterator<JsonNode> itr = array.iterator();
+            JsonNode emailNode = itr.next();
 
-        String email = externalUserInJson.get("email").asText();
-        result.setEmail(email);
+            result.setEmail(emailNode.asText());
+            result.setTenantName(emailNode.asText());
+        }
 
-        String domain = email .substring(email .indexOf("@") + 1);
-
-        // Any attribute from the external user info object can be used as tenant name
-        result.setTenantName("Tenant " + domain);
-
-        // You can set directly Tenant ID instead of the Tenant Name
-        // result.setTenantId(new TenantId(UUID.fromString("bc89e8f0-8ba9-11ea-97a9-0bc52285619a")));
-
-        String firstName = externalUserInJson.get("givenName").asText();
+        String firstName = externalUserInJson.get("given_name").asText();
         result.setFirstName(firstName);
 
-        String lastName = externalUserInJson.get("familyName").asText();
+        String lastName = externalUserInJson.get("family_name").asText();
         result.setLastName(lastName);
 
-
-//        // If required, you can set Customer Name, and user is going to be created on the Customer level
-//        String customerName = externalUserInJson.get("CUSTOMER_NAME_ATTRIBUTE").asText();
-//        result.setCustomerName(email);
-
-
-//        // You can set directly Customer ID instead of the Customer Name
-//        result.setCustomerId(new CustomerId(UUID.fromString("e78c0ab0-8ba9-11ea-97a9-0bc52285619a")));
-
-        // ------
-        // Fields applicable for Professional Edition
-        // ------
-
-//        // If required, you can set Parent Customer Name, and Customer of the user is going to be created in the hierarchy under the Parent Customer
-//        String parentCustomerName = externalUserInJson.get("PARENT_CUSTOMER_NAME_ATTRIBUTE").asText();
-//        result.setParentCustomerName(parentCustomerName);
-//        // You can set directly Parent Customer ID instead of the Parent Customer Name
-//        result.setParentCustomerId(new CustomerId(UUID.fromString("e78c0ab0-8ba9-11ea-97a9-0bc52285619a")));
-
-//        // If required, you can set user groups, where user is going to be assigned
-//        ArrayList<String> userGroups = new ArrayList<>();
-//        userGroups.add("Managers");
-//        userGroups.add("Tenant Administrators");
-//        userGroups.add(externalUserInJson.get("CUSTOMER_USER_GROUP").asText());
-//        result.setUserGroups(userGroups);
+        ArrayList<String> userGroups = new ArrayList<>();
+        userGroups.add("Tenant Administrators");
+        result.setUserGroups(userGroups);
 
         return result;
     }
